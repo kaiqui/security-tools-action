6ab49cc78de2cbb8ecdc3c0afac9f5fa1647c09e
Kaique Alves Lima <kaiqui82@gmail.com>
2023-10-28 23:37:07 -0300

    fix bandit and add logger

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import subprocess
+from loguru import logger
 import sys
 import os
 from dependency_check.main import run_dependency_check
 
 def install_dependencies():
+    logger.info("Instalando dependências...")
     subprocess.run(["python", "-m", "pip", "install", "--upgrade", "pip"], check=True)
     subprocess.run(["pip", "install", "bandit", "checkov"], check=True)
+    logger.success("Dependências instaladas com sucesso.")
 
 def run_tool(tool):
+    logger.info(f"Executando ferramenta: {tool}")
     if tool == "dependency-check":
         run_dependency_check()
     elif tool == "bandit":
-        subprocess.run(["python", "-m", "bandit", "-r", ".", "-f", "json", "-o", "reports/bandit-report.json"], check=True)
+        subprocess.run(["bandit", "-r", ".", "-f", "json", "-o", "bandit-report.json"], check=True)
     elif tool == "checkov":
-        subprocess.run(["checkov", "-d", ".", "-o", "json", "--output-file", "reports/checkov-report.json"], check=True)
+        subprocess.run(["checkov", "-d", ".", "-o", "json", "--output-file", "checkov-report.json"], check=True)
     else:
-        print(f"Tool {tool} not recognized")
+        logger.error(f"Ferramenta {tool} não reconhecida")
         sys.exit(1)
+    logger.success(f"Ferramenta {tool} executada com sucesso.")
 
 def main():
+    logger.info("Iniciando o script de ferramentas de segurança...")
     install_dependencies()
 
     tools_input = os.getenv("INPUT_TOOLS", "all")
     tools = {
-        "all": ["bandit", "checkov"],
-        "web": [ "bandit"],
+        "all": ["dependency-check", "bandit", "checkov"],
+        "web": ["bandit"],
         "iac": ["checkov"],
         "mobile": [],
         "android": [],
@@ -34,5 +40,21 @@ def main():
     for tool in tools:
         run_tool(tool.strip())
 
+    logger.success("Todas as ferramentas foram executadas com sucesso.")
+
 if __name__ == "__main__":
+    logger.info(
+        """\n
+ _______  _______  _______  _______  ___   _______  _______  _______  _______  _______  ___      _______ 
+|       ||       ||       ||       ||   | |       ||       ||       ||       ||       ||   |    |       |
+|  _____||    ___||       ||    _  ||   | |    _  ||    ___||_     _||   _   ||   _   ||   |    |  _____|
+| |_____ |   |___ |       ||   |_| ||   | |   |_| ||   |___   |   |  |  | |  ||  | |  ||   |    | |_____ 
+|_____  ||    ___||      _||    ___||   | |    ___||    ___|  |   |  |  |_|  ||  |_|  ||   |___ |_____  |
+ _____| ||   |___ |     |_ |   |    |   | |   |    |   |___   |   |  |       ||       ||       | _____| |
+|_______||_______||_______||___|    |___| |___|    |_______|  |___|  |_______||_______||_______||_______|
+        
+Author: kaiqui 
+Version: 0.1
+        \n"""
+    )
     main()
