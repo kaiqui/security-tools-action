c82a8a0703c4a16756d78dafc6fb56254cf5d368
Kaique Alves Lima <kaiqui82@gmail.com>
2023-10-29 08:07:37 -0300

    add docker tool

--- trufflehog/main.py
+++ trufflehog/main.py
@@ -12,8 +12,8 @@ def find_leaks():
     app = os.getenv("GITHUB_REPOSITORY").split("/")[-1]
     
     conn = f'https://{GH_LOGIN}:{TOKEN}@github.com/{org}/{app}'
-    cmd = (f'{PATH}/trufflehog git {conn} --json',
-           '--no-update > trufflehog.json')
+    cmd = (f'docker run --rm -it -v "$PWD:/pwd" trufflesecurity/trufflehog:latest git {conn} --json',
+           '> trufflehog.json')
     cmd = " ".join(str(item) for item in cmd)
     logger.info(cmd)
     result = subprocess.run(cmd, shell=True)
@@ -22,18 +22,22 @@ def find_leaks():
             out_json = json.loads(
                 "[" + f.read().replace("}\n{", "},\n{") + "]")
         for j in range(len(out_json)):
-            commit_hash = out_json[j]['SourceMetadata']['Data']['Git']['commit']
-            date_commit = out_json[j]['SourceMetadata']['Data']['Git']['timestamp']
-            date_commit = date_commit[0:19]
-            path = out_json[j]['SourceMetadata']['Data']['Git']['file']
-            reason = out_json[j]['DetectorName']
-            payload = {
-                'commit_hash': commit_hash,
-                'path': path,
-                'reason': reason,
-                'leak': out_json[j]['Raw']
-            }
-            logger.info(f"Leak found: {payload}")
+            try:
+                commit_hash = out_json[j]['SourceMetadata']['Data']['Git']['commit']
+                date_commit = out_json[j]['SourceMetadata']['Data']['Git']['timestamp']
+                date_commit = date_commit[0:19]
+                path = out_json[j]['SourceMetadata']['Data']['Git']['file']
+                reason = out_json[j]['DetectorName']
+                payload = {
+                    'commit_hash': commit_hash,
+                    'path': path,
+                    'reason': reason,
+                    'leak': out_json[j]['Raw']
+                }
+                logger.info(f"Leak found: {payload}")
+            except Exception as e:
+                logger.warning(e)
+                continue
     else:
         logger.error(f'RETURN CODE: {result.returncode}')
         return False
