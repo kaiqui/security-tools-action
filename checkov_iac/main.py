665ea42740c5cee372637ab97e06d012ba76fe6a
Kaique Alves Lima <kaiqui82@gmail.com>
2023-10-30 23:34:16 -0300

    fix list

--- checkov_iac/main.py
+++ checkov_iac/main.py
@@ -16,19 +16,20 @@ def iac_test():
             result_iac = json.loads(content)
             logger.info(result_iac)
             if result_iac:
-                failed_checks = result_iac['results']['failed_checks']
-                for check in failed_checks:
-                    check_id = check.get('check_id', 'Não informado')
-                    check_name = check.get('check_name', 'Não informado')
-                    file_path = check.get('file_path', 'Não informado')
-                    file_line_range = check.get('file_line_range', 'Não informado')
-                    guideline = check.get('guideline', 'Não informado')
-                    
-                    logger.error(f"Check ID: {check_id}\n"
-                                 f"Check Name: {check_name}\n"
-                                 f"File Path: {file_path}\n"
-                                 f"File Line Range: {file_line_range}\n"
-                                 f"Guideline: {guideline}")
+                for iac in result_iac:
+                    failed_checks = iac['results']['failed_checks']
+                    for check in failed_checks:
+                        check_id = check.get('check_id', 'Não informado')
+                        check_name = check.get('check_name', 'Não informado')
+                        file_path = check.get('file_path', 'Não informado')
+                        file_line_range = check.get('file_line_range', 'Não informado')
+                        guideline = check.get('guideline', 'Não informado')
+                        
+                        logger.error(f"Check ID: {check_id}\n"
+                                    f"Check Name: {check_name}\n"
+                                    f"File Path: {file_path}\n"
+                                    f"File Line Range: {file_line_range}\n"
+                                    f"Guideline: {guideline}")
             else:
                 logger.warning(f"Chaves 'results' ou 'failed_checks' não encontradas no arquivo de resultado {result_iac}")
     else:
