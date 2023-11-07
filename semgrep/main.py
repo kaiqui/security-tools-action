a390881ad784a7dcbafe3584422417f5e14bc51e
Kaique Alves Lima <kaiqui82@gmail.com>
2023-11-07 14:12:35 -0300

    add depcheck

--- semgrep/main.py
+++ semgrep/main.py
@@ -4,8 +4,8 @@ from loguru import logger
 import sys
 
 def sast_scan():
-    SEMGREP_APP_TOKEN = os.environ.get("SEMGREP_APP_TOKEN")
-    if SEMGREP_APP_TOKEN:
-        return subprocess.run(["docker", "run", f"SEMGREP_APP_TOKEN={SEMGREP_APP_TOKEN}", "--rm", "-v", '"${PWD}:/src"', "returntocorp/semgrep", "ci"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+    SEMGREP_TOKEN = os.environ.get("SEMGREP_TOKEN")
+    if SEMGREP_TOKEN:
+        return subprocess.run(["docker", "run", f"SEMGREP_APP_TOKEN={SEMGREP_TOKEN}", "--rm", "-v", '"${PWD}:/src"', "returntocorp/semgrep", "ci"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
     logger.error('SEMGREP_APP_TOKEN is not defined')
     sys.exit(1)
