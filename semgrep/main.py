8fc3064d7f984773a284a6b4aadf67f882b7a30c
Kaique Alves Lima <kaiqui82@gmail.com>
2023-11-07 14:25:51 -0300

    fix

--- semgrep/main.py
+++ semgrep/main.py
@@ -5,7 +5,8 @@ import sys
 
 def sast_scan():
     SEMGREP_TOKEN = os.getenv("SEMGREP_TOKEN")
-    if SEMGREP_TOKEN:
+    try:
         return subprocess.run(["docker", "run", f"SEMGREP_APP_TOKEN={SEMGREP_TOKEN}", "--rm", "-v", '"${PWD}:/src"', "returntocorp/semgrep", "ci"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
-    logger.error('SEMGREP_TOKEN is not defined')
-    sys.exit(1)
+    except:
+        logger.error('SEMGREP_TOKEN is not defined')
+        sys.exit(1)
